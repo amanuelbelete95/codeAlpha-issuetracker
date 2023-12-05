84a38e3d859a2c4c0af4f5de5d8074ef8582484b
Amanuel Belete <amanuelbelete95@gmail.com>
2023-12-05 10:59:15 +0300

    Add issue tracker functinality

--- script.js
+++ script.js
@@ -1,64 +1,202 @@
 const issueInput = document.querySelector('.issue-input');
-const openIssueContainer = document.querySelector('.open-list');
+const openList = document.querySelector('.open-list');
+const progressList = document.querySelector('.progress-list');
+const completeList = document.querySelector('.complete-list');
 const issueForm = document.querySelector('.issue-form');
 
 const descriptionText = document.querySelector('.description-text');
 
-console.log(descriptionText);
+// save to local storage of open-issue;
+function saveToLocalStorage(title, description) {
+  const storedOpenIssues = localStorage.getItem('open-issues')
+    ? JSON.parse(localStorage.getItem('open-issues'))
+    : [];
+  storedOpenIssues;
+
+  const newIssue = {
+    title: title,
+    description: description,
+  };
+
+  storedOpenIssues.push(newIssue);
+
+  localStorage.setItem('open-issues', JSON.stringify(storedOpenIssues));
+}
+//save to localstorage of progress;
+const saveToLocalStorageProgress = (title, description) => {
+  const storedProgress = localStorage.getItem('progress-issues')
+    ? JSON.parse(localStorage.getItem('progress-issues'))
+    : [];
+
+  const newProgessIssue = {
+    title: title,
+    description: description,
+  };
+
+  storedProgress.push(newProgessIssue);
+  localStorage.setItem('progress-issues', JSON.stringify(storedProgress));
+};
+
+const saveToLocalStorageComplete = (title, description) => {
+  const storedComplete = localStorage.getItem('complete-issues')
+    ? JSON.parse(localStorage.getItem('complete-issues'))
+    : [];
+
+  const newCompleteIssues = {
+    title: title,
+    description: description,
+  };
+
+  storedComplete.push(newCompleteIssues);
+  localStorage.setItem('complete-issues', JSON.stringify(storedComplete));
+};
 
 function addIssue(e) {
   e.preventDefault();
   const issueTitle = issueInput.value;
   const span = document.createElement('span');
   span.innerHTML = issueTitle;
+  span.classList.add('issue-title-class');
 
-  // Create a div to add description to the issues
   const descriptionDiv = document.createElement('div');
-  descriptionDiv.innerHTML = `<strong>Descriptions</strong>: ${descriptionText.value}`;
+  let description = descriptionText.value;
+  descriptionDiv.innerHTML = `<strong>Descriptions</strong>: <br/>${description}`;
 
   const li = document.createElement('li');
   li.appendChild(span);
   li.appendChild(descriptionDiv);
+  li.classList.add('open');
 
-  openIssueContainer.appendChild(li);
-
-  // input.value = '';
-  console.log(`${descriptionText.value}`);
+  openList.appendChild(li);
 
   issueInput.value = '';
   descriptionText.value = '';
+
+  saveToLocalStorage(issueTitle, description);
+}
+
+const moveToProgress = (e) => {
+  if (e.target.parentNode.tagName === 'LI') {
+    const li = e.target.parentNode;
+    li.classList.add('progress');
+
+    //remove it from the Ui;
+    openList.removeChild(li);
+
+    const Data = JSON.parse(localStorage.getItem('open-issues'));
+
+    const upDatedOpenList = Data.filter(
+      (item) => item.title !== li.firstElementChild.textContent
+    );
+
+    localStorage.setItem('open-issues', JSON.stringify(upDatedOpenList));
+
+    //all about progress ui and local;
+    progressList.appendChild(li);
+    li.style.backgroundColor = '#00ff00';
+    const issueTitle = li.firstElementChild.textContent;
+    const desc = li.lastElementChild.textContent;
+    saveToLocalStorageProgress(issueTitle, desc);
+  }
+};
+function moveToComplete(e) {
+  if (e.target.parentNode.tagName === 'LI') {
+    const li = e.target.parentNode;
+    li.classList.add('complete');
+
+    //remove it from the Ui;
+    progressList.removeChild(li);
+
+    //remove it from local storage here;
+
+    const Data = JSON.parse(localStorage.getItem('progress-issues'));
+
+    const updatedProgressList = Data.filter(
+      (item) => item.title !== li.firstElementChild.textContent
+    );
+
+    localStorage.setItem(
+      'progress-issues',
+      JSON.stringify(updatedProgressList)
+    );
+
+    completeList.appendChild(li);
+    li.style.backgroundColor = '#44444';
+    const issueTitle = li.firstElementChild.textContent;
+    const desc = li.lastElementChild.textContent;
+    saveToLocalStorageComplete(issueTitle, desc);
+  }
 }
 
 issueForm.addEventListener('submit', addIssue);
+openList.addEventListener('click', moveToProgress);
+progressList.addEventListener('click', moveToComplete);
 
-/*
+//add to user interface of open list;
 
-const issueInput = document.querySelector('.issue-input');
-const OpenIssueContainer = document.querySelector('.open-list');
-const addBtn = document.querySelector('.add-btn');
-const issueForm = document.querySelector('.issue-form');
+const UpdateUIOpen = () => {
+  const storedIssues = localStorage.getItem('open-issues');
+  if (storedIssues) {
+    const Data = JSON.parse(localStorage.getItem('open-issues')) || [];
 
-function addIssue(e) {
-  e.preventDefault();
-  const input = e.target.firstElementChild;
-  const span = document.createElement('span');
-  span.innerHTML = input.value;
-  const li = document.createElement('li');
-  li.appendChild(span);
-  OpenIssueContainer.appendChild(li);
+    Data.forEach((data) => {
+      const li = document.createElement('li');
+      const span = document.createElement('span');
+      span.innerHTML = data.title;
+      span.classList.add('issue-title-class');
+      const div = document.createElement('div');
+      div.innerHTML = data.description;
+      li.appendChild(span);
+      li.appendChild(div);
+      openList.appendChild(li);
+    });
+  }
+};
 
-  //create a div to add description to the issues;
-}
+const UpdatedUIProgress = () => {
+  const storedProgressIssues = localStorage.getItem('progress-issues');
+  if (storedProgressIssues) {
+    const data = JSON.parse(localStorage.getItem('progress-issues')) || [];
 
-issueForm.addEventListener('submit', addIssue);
+    data.forEach((item) => {
+      const li = document.createElement('li');
 
-function spanClick(e) {
-  console.log("So, you've clicked the span.");
-  div.appendChild(this);
-}
+      const span = document.createElement('span');
+      span.innerHTML = item.title;
+      span.classList.add('issue-title-class');
 
-var span = document.getElementById('demo'),
-  div = document.getElementById('recipient');
+      const div = document.createElement('div');
+      div.innerHTML = item.description;
 
-span.addEventListener('click', spanClick);
-*/
+      li.appendChild(span);
+      li.appendChild(div);
+      progressList.appendChild(li);
+    });
+  }
+};
+
+const UpdatedUIComplete = () => {
+  const storedCompleteIssues = localStorage.getItem('complete-issues');
+  if (storedCompleteIssues) {
+    const data = JSON.parse(localStorage.getItem('complete-issues')) || [];
+
+    data.forEach((item) => {
+      const li = document.createElement('li');
+
+      const span = document.createElement('span');
+      span.innerHTML = item.title;
+      span.classList.add('issue-title-class');
+
+      const div = document.createElement('div');
+      div.innerHTML = item.description;
+
+      li.appendChild(span);
+      li.appendChild(div);
+      completeList.appendChild(li);
+    });
+  }
+};
+
+UpdateUIOpen();
+UpdatedUIProgress();
+UpdatedUIComplete();
